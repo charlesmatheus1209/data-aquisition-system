fe44f87e823bb7df6b7ea0c3f97035a6440f7ad4
charlesmatheus1209 <98367642+charlesmatheus1209@users.noreply.github.com>
2024-06-20 17:47:18 +0000

    Finalização sem testes

--- src/main.cpp
+++ src/main.cpp
@@ -4,39 +4,109 @@
 #include <utility>
 #include <boost/asio.hpp>
 #include <fstream>
-
+#include <vector>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 
 using boost::asio::ip::tcp;
 
-struct record_t
-{
-	 int  id;
-	 char name[16];
-	 char phone[16]; // 3134679819
+struct LogRecord {
+    char sensor_id[32]; // supondo um ID de sensor de até 32 caracteres
+    std::time_t timestamp; // timestamp UNIX
+    double value; // valor da leitura
 };
 
-class file {
 
-    public:
-    std::string filename = "";
+std::time_t string_to_time_t(const std::string& time_string) {
+    std::tm tm = {};
+    std::istringstream ss(time_string);
+    ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%S");
+    return std::mktime(&tm);
+}
+
+std::string time_t_to_string(std::time_t time) {
+    std::tm* tm = std::localtime(&time);
+    std::ostringstream ss;
+    ss << std::put_time(tm, "%Y-%m-%dT%H:%M:%S");
+    return ss.str();
+}
+
+std::vector<std::string> splitString(const std::string& str, char delimiter) {
+    std::vector<std::string> result;
+    std::stringstream ss(str);
+    std::string item;
     
-    file::file(std::string filename){
-        this->filename = filename;
+    while (getline(ss, item, delimiter)) {
+        result.push_back(item);
+    }
+    
+    return result;
+}
+
+class file {
+public:
+    std::string filename;
+
+    // Construtor
+    file(std::string filename) {
+        this->filename = "../src/" + filename + ".dat";
     }
 
-    void file::append_in_file(){
-        std::fstream file(filename, std::fstream::out | std::fstream::in | std::fstream::binary 
-																	 | std::fstream::app); 
-
-        if (file.is_open()){
-            std::cout << "Arquivo sendo escrito...." << std::endl;
-        }else{
-            std::cout << "Error opening file!" << std::endl;
-        }                                                             
+    // Método para append em arquivo
+    bool append_in_file(LogRecord record) {
+      std::fstream file(filename, std::fstream::out | std::fstream::in | std::fstream::binary | std::fstream::app); 
+      if (file.is_open()) {
+          std::cout << "Arquivo sendo escrito...." << std::endl;
+          file.write((char*)&record, sizeof(record));
+          file.close();
+          return true;
+      } else {
+          std::cout << "Error opening file!" << std::endl;
+          return false;
+      }
     }
 
-    void file::read_file(){
-        std::cout << "Arquivo sendo lido...." << std::endl;
+    // Método para leitura de arquivo
+    std::string read_file(int number) {
+      std::fstream file(filename, std::fstream::in | std::fstream::binary); 
+      if (file.is_open()) {
+          std::cout << "Arquivo sendo lido...." << std::endl;
+          
+          // Vai para o fim do arquivo para determinar o tamanho
+          file.seekg(0, std::ios::end);
+          int file_size = file.tellg();
+          
+          // Calcula o número de registros presentes no arquivo
+          int n = file_size / sizeof(LogRecord);
+
+          if (n == 0) {
+              std::cout << "Arquivo vazio." << std::endl;
+              file.close();
+              return "";
+          }
+
+          int id = (n > number) ? n - number : 0;
+          file.seekg(id * sizeof(LogRecord), std::ios_base::beg);
+
+          int qtd = (n > number) ? number : n;
+          std::string retorno = std::to_string(qtd) + ";";
+          // Lê e imprime os registros selecionados
+          for (int i = id; i < n; ++i) {
+              LogRecord rec;
+              file.read((char*)&rec, sizeof(LogRecord));
+              std::cout << "Id: " << rec.sensor_id << " - time: " << time_t_to_string(rec.timestamp) << " - value: " << rec.value << std::endl;
+              retorno += time_t_to_string(rec.timestamp) + "|" + std::to_string(rec.value) + ";";
+          }
+          retorno.erase(retorno.length() - 1);
+          retorno += "\r\n";
+
+          file.close();
+          return retorno;
+      } else {
+          std::cout << "Arquivo não sendo lido...." << std::endl;
+          return "";
+      }
     }
 };
 
@@ -68,21 +138,37 @@ private:
             std::istream is(&buffer_);
             std::string message(std::istreambuf_iterator<char>(is), {});
             std::cout << "Received: " << message << std::endl;
+            
+            std::vector<std::string> message_splited = splitString(message, '|');
 
-            if(message.size() >= 3){
-                std::string type = message.substr(0, 3);
-                if(type == "GET"){
-                    std::cout << "Essa mensagem é do tipo GET" << std::endl;
-                    write_message("Foi um GET");
-                }else if(type == "LOG"){
+            if(message_splited.size() >= 3){
+                if(message_splited[0] == "GET"){
+                    std::cout << "Essa mensagem é do tipo GET" << std::endl;                  
+
+                    file f(message_splited[1]);
+                    std::string retorno = f.read_file(stoi(message_splited[2]));
+
+                    if(retorno != ""){
+                      write_message(retorno);
+                    }else{
+                      write_message("ERROR|INVALID_SENSOR_ID\r\n");
+                    }
+
+                }else if(message_splited[0] == "LOG"){
                     std::cout << "Essa mensagem é do tipo LOG" << std::endl;
-                    write_message("Foi um LOG");
+                    LogRecord record; 
+                    std::strncpy(record.sensor_id, message_splited[1].c_str(), sizeof(record.sensor_id) - 1);
+                    record.timestamp = string_to_time_t(message_splited[2]);
+                    record.value = stod(message_splited[3]);
+
+                    file f(message_splited[1]);
+                    bool ret = f.append_in_file(record);
+                    
+                    read_message();
                 }else{
                     std::cout << "A mensagem não foi entendida pelo servidor" << std::endl;
                     write_message("ERROR|INVALID REQUEST\r\n");
                 }
-                
-
             }else{
                 write_message("ERROR|INVALID REQUEST\r\n");
             }
@@ -136,15 +222,15 @@ private:
 
 int main(int argc, char* argv[])
 {
-  if (argc != 2)
+  /*if (argc != 2)
   {
     std::cerr << "Usage: chat_server <port>\n";
     return 1;
-  }
+  }*/
 
   boost::asio::io_context io_context;
 
-  server s(io_context, std::atoi(argv[1]));
+  server s(io_context, 9000/*std::atoi(argv[1])*/);
 
   io_context.run();
 
